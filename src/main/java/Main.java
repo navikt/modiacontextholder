fad81b24c3853c89a8c1a3b7bd73f96aa5580413
Nicklas Utgaard <nicklas.utgaard@nav.no>
2019-08-29 15:54:55 +0200

    [KAIZEN-0] la til cors-headers for prod

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import no.nav.apiapp.ApiApp;
 import no.nav.sbl.config.ApplicationConfig;
 
 import static no.nav.apiapp.rest.NavCorsFilter.CORS_ALLOWED_ORIGINS;
+import static no.nav.sbl.util.EnvironmentUtils.EnviromentClass.P;
 import static no.nav.sbl.util.EnvironmentUtils.EnviromentClass.Q;
 import static no.nav.sbl.util.EnvironmentUtils.Type.PUBLIC;
 import static no.nav.sbl.util.EnvironmentUtils.isEnvironmentClass;
@@ -16,7 +17,9 @@ public class Main {
     }
 
     private static void setCors() {
-        if (isEnvironmentClass(Q)) {
+        if (isEnvironmentClass(P)) {
+            setProperty(CORS_ALLOWED_ORIGINS, ".adeo.no", PUBLIC);
+        } else if (isEnvironmentClass(Q)) {
             String subDomain = ".preprod.local";
             log.info("Setting CORS-headers to {}, YOU SHOULD NOT SEE THIS IN PRODUCTION!!!", subDomain);
             setProperty(CORS_ALLOWED_ORIGINS, subDomain, PUBLIC);
