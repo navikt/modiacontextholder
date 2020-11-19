b05f423e65d2b15a1ddca3ba4633205ea96fa9fd
Andreas Bergman <andreasbergman93@gmail.com>
2020-11-19 09:36:13 +0100

    [KAIZEN-0] oppdaterte miljovariabel

--- src/main/java/no/nav/sbl/config/FeatureToggleConfig.java
+++ src/main/java/no/nav/sbl/config/FeatureToggleConfig.java
@@ -2,19 +2,20 @@ package no.nav.sbl.config;
 
 import no.nav.common.featuretoggle.UnleashService;
 import no.nav.common.featuretoggle.UnleashServiceConfig;
+import no.nav.common.utils.EnvironmentUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeatureToggleConfig {
 
-    private static String UNLEASH_API_URL = "https://unleash.nais.io/api/";
+    private static String UNLEASH_URL = EnvironmentUtils.getRequiredProperty("UNLEASH_API_URL");;
 
     @Bean
     public UnleashService unleashService() {
         UnleashServiceConfig config = UnleashServiceConfig.builder()
                 .applicationName("modiacontextholder")
-                .unleashApiUrl(UNLEASH_API_URL)
+                .unleashApiUrl(UNLEASH_URL)
                 .build();
 
         return new UnleashService(config);
