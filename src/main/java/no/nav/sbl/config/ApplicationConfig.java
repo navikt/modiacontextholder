bb479a5225de2cc56d5c8d3a8e81143a54f9d68b
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-06-16 13:37:31 +0200

    Legg til ClientId for Syfomoteoversikt

--- src/main/java/no/nav/sbl/config/ApplicationConfig.java
+++ src/main/java/no/nav/sbl/config/ApplicationConfig.java
@@ -46,6 +46,7 @@ public class ApplicationConfig {
     private static final String azureADClientId = EnvironmentUtils.getRequiredProperty("LOGINSERVICE_OIDC_CLIENTID");
     private static final String veilarbloginAADClientId = EnvironmentUtils.getRequiredProperty("VEILARBLOGIN_AAD_CLIENT_ID");
     private static final String syfoFinnfastlegeClientId = EnvironmentUtils.getRequiredProperty("SYFO_FINNFASTLEGE_CLIENTID");
+    private static final String syfoSyfomoteoversiktClientId = EnvironmentUtils.getRequiredProperty("SYFO_SYFOMOTEOVERSIKT_CLIENTID");
     private static final String syfoSyfooversiktClientId = EnvironmentUtils.getRequiredProperty("SYFO_SYFOOVERSIKT_CLIENTID");
     private static final String syfoSmregClientId = EnvironmentUtils.getRequiredProperty("SYFO_SMREG_CLIENTID");
     private static final String syfoSmmanuellClientId = EnvironmentUtils.getRequiredProperty("SYFO_SMMANUELL_CLIENTID");
@@ -78,7 +79,7 @@ public class ApplicationConfig {
                 .withUserRole(UserRole.INTERN);
 
         OidcAuthenticatorConfig azureAdV2 = new OidcAuthenticatorConfig()
-                .withClientIds(asList(syfoFinnfastlegeClientId, syfoSyfooversiktClientId, syfoSmregClientId, syfoSmmanuellClientId, sosialhjelpModiaClientId, veilarbloginAADClientId))
+                .withClientIds(asList(syfoFinnfastlegeClientId, syfoSyfomoteoversiktClientId, syfoSyfooversiktClientId, syfoSmregClientId, syfoSmmanuellClientId, sosialhjelpModiaClientId, veilarbloginAADClientId))
                 .withDiscoveryUrl(azureADV2DiscoveryUrl)
                 .withIdTokenCookieName(Constants.AZURE_AD_ID_TOKEN_COOKIE_NAME)
                 .withUserRole(UserRole.INTERN);
