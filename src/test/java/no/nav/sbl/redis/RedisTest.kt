3216d550b13b9c84392dd97f44c1106144303f25
Eirik Dahlen <dahleneirik@gmail.com>
2022-01-04 11:29:22 +0100

    [OPP-1408] lage TestUtil for Redis med låsemekanisme
    Ved testing av publisering og mottak av meldinger i Redis oppstår det mulige feilkilder: jedis.psubscribe er et blokkerende kall, docker-container har kanskje ikke startet i det man sender en melding, og en melding er nødvendigvis ikke prosessert, sendt og mottatt i det man skal asserte mottatte meldinger.
    
    Ved å innføre en låsemekanisme basert på mutual exclusion kan man kontrollere at ting skjer i riktig rekkefølge uten å måtte legge inn delays på x antall sekunder.

--- src/test/java/no/nav/sbl/redis/RedisTest.kt
+++ src/test/java/no/nav/sbl/redis/RedisTest.kt
@@ -1,83 +1,25 @@
 package no.nav.sbl.redis
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import no.nav.common.health.HealthCheckResult
-import org.junit.*
-import org.junit.Assert.*
-import org.testcontainers.containers.GenericContainer
-import redis.clients.jedis.HostAndPort
-import redis.clients.jedis.Jedis
-import redis.clients.jedis.JedisPubSub
-
-object TestUtils {
-    class RedisContainer : GenericContainer<RedisContainer>("redis:6-alpine")
-    
-    class RedisSubscriber : JedisPubSub() {
-        val messages: MutableList<Pair<String, String>> = mutableListOf()
-        
-        override fun onPMessage(pattern: String, channel: String, message: String) {
-            messages.add(Pair(channel, message))
-        }
-    }
-    
-    interface WithRedis {
-        
-        companion object {
-            private var job: Job? = null
-            private var subscriber: RedisSubscriber? = null
-            private val container = RedisContainer()
-            
-            
-            @BeforeClass
-            @JvmStatic
-            fun startContainer() {
-                container.start()
-            }
-            
-            @AfterClass
-            @JvmStatic
-            fun stopContainer() {
-                container.stop()
-            }
-        }
-        
-        @Before
-        fun setupSubscriber() {
-            subscriber = RedisSubscriber()
-            val jedis = Jedis(redisHostAndPort())
-            job = GlobalScope.launch {
-                jedis.psubscribe(subscriber, "*")
-            }
-        }
-        
-        @After
-        fun closeSubscriber() {
-            subscriber?.unsubscribe()
-            job?.cancel()
-            subscriber = null
-            job = null
-        }
-        
-        fun redisHostAndPort() = HostAndPort(container.host, container.getMappedPort(6379))
-        
-        fun getMessages() = subscriber?.messages ?: emptyList<Pair<String, String>>()
-    }
-}
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertTrue
 
 class RedisTest : TestUtils.WithRedis {
-    
     private val redisPublisher = Redis.Publisher(redisHostAndPort())
     
     @Test
-    fun `sender redis-melding`() {
-        redisPublisher.publishMessage("Test", "Test")
-        assertEquals("Test", getMessages()[0])
+    fun `sender redis-meldinger`() = runBlocking {
+        redisPublisher.publishMessage("TestChannel", "TestMessage1")
+        redisPublisher.publishMessage("TestChannel", "TestMessage2")
+        redisPublisher.publishMessage("TestChannel", "TestMessage3")
+        assertReceivedMessage("TestChannel", "TestMessage3")
+        assertReceivedMessage("TestChannel", "TestMessage2")
+        assertReceivedMessage("TestChannel", "TestMessage1")
+        assertTrue(getMessages().size == 3)
     }
     
     @Test
     fun `redis selfcheck ok`() {
-        assertEquals(redisPublisher.checkHealth(), HealthCheckResult.healthy())
+        assertTrue(redisPublisher.checkHealth().isHealthy)
     }
 }
