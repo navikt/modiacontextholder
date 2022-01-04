bfd50fb0adaa9409a42c23790d80624993848e41
Eirik Dahlen <dahleneirik@gmail.com>
2022-01-04 12:13:45 +0100

    [OPP-1408] logge hvilken melding som blir sendt fra redis

--- src/main/java/no/nav/sbl/redis/RedisPublisher.kt
+++ src/main/java/no/nav/sbl/redis/RedisPublisher.kt
@@ -4,16 +4,24 @@ import no.nav.common.health.HealthCheck
 import no.nav.common.health.HealthCheckResult
 import no.nav.common.health.selftest.SelfTestCheck
 import no.nav.sbl.config.Pingable
+import org.slf4j.LoggerFactory
 import redis.clients.jedis.HostAndPort
 import redis.clients.jedis.Jedis
 
 object Redis {
     
-    class Publisher(private val hostAndPort: HostAndPort) : HealthCheck, Pingable   {
+    class Publisher(private val hostAndPort: HostAndPort) : HealthCheck, Pingable {
+        private val logger = LoggerFactory.getLogger(Publisher::class.java)
         private val jedis = Jedis(hostAndPort)
         
         fun publishMessage(channel: String, message: String) {
             jedis.publish(channel, message)
+            logger.info(
+                """
+                Redismelding sendt på kanal '$channel' med melding:
+                $message
+                """.trimIndent()
+            )
         }
     
         override fun checkHealth(): HealthCheckResult {
