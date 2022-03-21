b3178ad16226ada02635433e640b23602247f043
Nicklas Utgaard <nutgaard@gmail.com>
2022-03-21 11:26:42 +0100

    [KAIZEN-0] legge til azureAd config og inbound policies
    registrerer appen i azure, og lar andre lage OBO-token til modiacontextholder.

--- src/test/java/no/nav/sbl/ApplicationConfigTest.java
+++ src/test/java/no/nav/sbl/ApplicationConfigTest.java
@@ -1,6 +1,7 @@
 package no.nav.sbl;
 
 import no.nav.common.nais.NaisYamlUtils;
+import no.nav.common.utils.EnvironmentUtils;
 import org.junit.Test;
 
 import java.net.URL;
@@ -9,7 +10,14 @@ import java.net.URLClassLoader;
 public class ApplicationConfigTest {
     @Test
     public void appconfigKanLastesFraQ0Config() throws Exception {
+        injectAzureProperties();
         NaisYamlUtils.loadFromYaml(".nais/nais.yaml");
+        
         new URLClassLoader(new URL[]{}).loadClass("no.nav.sbl.config.ApplicationConfig").newInstance();
+    }
+
+    private void injectAzureProperties() {
+        EnvironmentUtils.setProperty("AZURE_APP_WELL_KNOWN_URL", "http://dummy.io", EnvironmentUtils.Type.PUBLIC);
+        EnvironmentUtils.setProperty("AZURE_APP_CLIENT_ID", "clientid", EnvironmentUtils.Type.PUBLIC);
     }
 }
