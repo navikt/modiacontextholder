47dba649dbf537ff3fb40b82000b61bdd4592454
Nicklas Utgaard <nutgaard@gmail.com>
2022-04-01 14:21:10 +0200

    [KAIZEN-0] fikset unleash setup
    unleash oppsett fra common har endret på seg, og trenger derfor en liten endring

--- src/main/java/no/nav/sbl/config/FeatureToggle.java
+++ src/main/java/no/nav/sbl/config/FeatureToggle.java
@@ -1,14 +1,14 @@
 package no.nav.sbl.config;
 
-import no.nav.common.featuretoggle.UnleashService;
+import no.nav.common.featuretoggle.UnleashClient;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class FeatureToggle {
 
-    private UnleashService unleashService;
+    private UnleashClient unleashService;
 
     @Autowired
-    public FeatureToggle(UnleashService unleashService) {
+    public FeatureToggle(UnleashClient unleashService) {
         this.unleashService = unleashService;
     }
 
