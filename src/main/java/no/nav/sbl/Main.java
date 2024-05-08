fc3b4cd44acc193ad5f74808c4a238c844c70c6f
abrha <abrha.kiros.abrha@nav.no>
2024-05-08 13:52:44 +0200

    [KAIZEN-0] litt clean up

--- src/main/java/no/nav/sbl/Main.java
+++ src/main/java/no/nav/sbl/Main.java
@@ -6,9 +6,6 @@ import no.nav.common.utils.NaisUtils;
 import no.nav.common.utils.SslUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Objects;
-
 import static no.nav.common.utils.EnvironmentUtils.Type.PUBLIC;
 import static no.nav.common.utils.EnvironmentUtils.Type.SECRET;
 import static no.nav.common.utils.EnvironmentUtils.setProperty;
@@ -19,24 +16,24 @@ import static no.nav.sbl.config.DatabaseConfig.*;
 @SpringBootApplication
 public class Main {
     public static void main(String... args) {
-        setupVault();
+        String clusterName = String.valueOf(EnvironmentUtils.getClusterName());
+        if(!GCP_CLUSTERS.contains(clusterName)) {
+            setupVault();
+        }
         SslUtils.setupTruststore();
         SpringApplication.run(Main.class, args);
     }
 
     private static void setupVault() {
-        String clusterName = String.valueOf(EnvironmentUtils.getClusterName());
-        if(!Objects.equals(clusterName, "dev-gcp") && !Objects.equals(clusterName, "prod-gcp")) {
-            Credentials serviceUser = NaisUtils.getCredentials("service_user");
-            setProperty(SRV_USERNAME_PROPERTY, serviceUser.username, PUBLIC);
-            setProperty(SRV_PASSWORD_PROPERTY, serviceUser.password, SECRET);
+        Credentials serviceUser = NaisUtils.getCredentials("service_user");
+        setProperty(SRV_USERNAME_PROPERTY, serviceUser.username, PUBLIC);
+        setProperty(SRV_PASSWORD_PROPERTY, serviceUser.password, SECRET);
 
-            Credentials dbCredentials = NaisUtils.getCredentials("modiacontextholderDB");
-            setProperty(MODIACONTEXTHOLDERDB_USERNAME, dbCredentials.username, PUBLIC);
-            setProperty(MODIACONTEXTHOLDERDB_PASSWORD, dbCredentials.password, SECRET);
+        Credentials dbCredentials = NaisUtils.getCredentials("modiacontextholderDB");
+        setProperty(MODIACONTEXTHOLDERDB_USERNAME, dbCredentials.username, PUBLIC);
+        setProperty(MODIACONTEXTHOLDERDB_PASSWORD, dbCredentials.password, SECRET);
 
-            String dbUrl = NaisUtils.getFileContent("/var/run/secrets/nais.io/db_config/jdbc_url");
-            setProperty(MODIACONTEXTHOLDERDB_URL_PROPERTY, dbUrl, PUBLIC);
-        }
+        String dbUrl = NaisUtils.getFileContent("/var/run/secrets/nais.io/db_config/jdbc_url");
+        setProperty(MODIACONTEXTHOLDERDB_URL_PROPERTY, dbUrl, PUBLIC);
     }
 }
