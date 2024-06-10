002a848bb375cdf61ac7d85450c7cc310b615741
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-06-10 13:28:28 +0200

    EventRessurs til Kotlin

--- src/main/java/no/nav/sbl/rest/EventRessurs.kt
+++ src/main/java/no/nav/sbl/rest/EventRessurs.kt
@@ -1,22 +1,24 @@
-package no.nav.sbl.rest;
+package no.nav.sbl.rest
 
-import no.nav.sbl.rest.domain.RSEvents;
-import no.nav.sbl.service.EventService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import no.nav.sbl.rest.domain.RSEvents
+import no.nav.sbl.service.EventService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/events")
-public class EventRessurs {
+class EventRessurs {
 
     @Autowired
-    private EventService eventService;
+    lateinit var eventService: EventService
 
     @GetMapping("{eventId}")
-    public RSEvents hentNyeEvents(@PathVariable("eventId") String eventId) {
-        return new RSEvents().events(eventService.hentEventerEtterId(Long.parseLong(eventId)));
+    fun hentNyeEvents(@PathVariable("eventId") eventId: String): RSEvents {
+        return RSEvents().apply {
+            events = eventService.hentEventerEtterId(eventId.toLong())
+        }
     }
 }
