22d58674d32fc5ba0fb3de97f16f66b7eda7a408
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-06-19 16:07:22 +0200

    Fiks test

--- src/test/java/no/nav/sbl/EventDAOTest.kt
+++ src/test/java/no/nav/sbl/EventDAOTest.kt
@@ -11,31 +11,34 @@ import org.springframework.jdbc.core.JdbcTemplate
 
 @RunWith(MockitoJUnitRunner::class)
 class EventDAOTest {
-
     private val jdbcTemplate: JdbcTemplate = mockk(relaxed = true)
-    private val eventDAO = EventDAO(jdbcTemplate, mockk(), mockk())
+    private val eventDAO = EventDAO(jdbcTemplate, mockk())
 
     @Test
     fun `slettAlleEventerUtenomNyeste sletter faktisk nyeste`() {
         val sqlQuery = mutableListOf<String>()
         val args = mutableListOf<Long>()
 
-        val event1 = PEvent().apply {
-            id = 1L
-            verdi = "1"
-        }
-        val event2 = PEvent().apply {
-            id = 2L
-            verdi = "2"
-        }
-        val event3 = PEvent().apply {
-            id = 3L
-            verdi = "3"
-        }
-        val event4 = PEvent().apply {
-            id = 4L
-            verdi = "4"
-        }
+        val event1 =
+            PEvent().apply {
+                id = 1L
+                verdi = "1"
+            }
+        val event2 =
+            PEvent().apply {
+                id = 2L
+                verdi = "2"
+            }
+        val event3 =
+            PEvent().apply {
+                id = 3L
+                verdi = "3"
+            }
+        val event4 =
+            PEvent().apply {
+                id = 4L
+                verdi = "4"
+            }
 
         eventDAO.slettAlleEventerUtenomNyeste(listOf(event1, event4, event2, event3))
 
