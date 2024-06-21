df887ffe8aba11d142ae4e6e21451669dcbe2f71
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-06-21 08:14:58 +0200

    Ta bort ubrukt funksjon

--- src/main/java/no/nav/sbl/db/dao/DbUtil.java
+++ src/main/java/no/nav/sbl/db/dao/DbUtil.java
@@ -1,24 +1,13 @@
 package no.nav.sbl.db.dao;
 
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.Optional;
 
-import static java.util.Optional.*;
+import static java.util.Optional.ofNullable;
 
 public class DbUtil {
-
-    public static <T> Optional<T> queryOptional(JdbcTemplate jdbcTemplate, String sql, RowMapper<T> rowMapper, Object... args) {
-        try {
-            return of(jdbcTemplate.queryForObject(sql, rowMapper, args));
-        } catch (DataAccessException e) {
-            return empty();
-        }
-    }
 
     public static long nesteSekvensverdi(String sekvensnavn, JdbcTemplate jdbcTemplate) {
         return jdbcTemplate.queryForObject("select " + sekvensnavn + ".nextval from dual", (rs, rowNum) -> rs.getLong(1));
