628f8cd6b86c7cad7c42c44b02e963c205b482df
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-06-21 08:34:52 +0200

    DbHelsesjekk til Kotlin

--- src/main/java/no/nav/sbl/db/DbHelsesjekk.kt
+++ src/main/java/no/nav/sbl/db/DbHelsesjekk.kt
@@ -1,45 +1,27 @@
-package no.nav.sbl.db;
+package no.nav.sbl.db
 
-import no.nav.common.health.HealthCheck;
-import no.nav.common.health.HealthCheckResult;
-import no.nav.common.health.selftest.SelfTestCheck;
-import no.nav.common.utils.EnvironmentUtils;
-import no.nav.sbl.config.Pingable;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import no.nav.common.health.HealthCheck
+import no.nav.common.health.HealthCheckResult
+import no.nav.common.health.selftest.SelfTestCheck
+import no.nav.common.utils.EnvironmentUtils
+import no.nav.sbl.config.Pingable
+import org.springframework.jdbc.core.JdbcTemplate
 
+class DbHelsesjekk(
+    private val jdbcTemplate: JdbcTemplate,
+) : HealthCheck,
+    Pingable {
+    override fun checkHealth(): HealthCheckResult =
+        runCatching {
+            jdbcTemplate.execute("SELECT * FROM DUAL")
+        }.fold(
+            onSuccess = { HealthCheckResult.healthy() },
+            onFailure = { HealthCheckResult.unhealthy(it) },
+        )
 
-public class DbHelsesjekk implements HealthCheck, Pingable {
-
-    private static final String MODIACONTEXTHOLDERDB_URL = "MODIACONTEXTHOLDERDB_URL";
-
-    private JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    public DbHelsesjekk(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    @Override
-    public HealthCheckResult checkHealth() {
-        try {
-            helsesjekk();
-            return HealthCheckResult.healthy();
-        } catch (Exception exception) {
-            return HealthCheckResult.unhealthy(exception);
-        }
-    }
-
-    public void helsesjekk() {
-        jdbcTemplate.execute("SELECT * FROM DUAL");
-    }
-
-    @Override
-    public SelfTestCheck ping() {
-        return new SelfTestCheck(
-                "dbhelsesjekk " + EnvironmentUtils.getRequiredProperty(MODIACONTEXTHOLDERDB_URL),
-                true,
-                this::checkHealth
-        );
-    }
+    override fun ping(): SelfTestCheck =
+        SelfTestCheck(
+            "dbhelsesjekk " + EnvironmentUtils.getRequiredProperty("MODIACONTEXTHOLDERDB_URL"),
+            true,
+        ) { this.checkHealth() }
 }
