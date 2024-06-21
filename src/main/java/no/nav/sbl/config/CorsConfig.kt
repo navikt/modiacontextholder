2936e9c6dc8ed170a0da34638682801a1e4d81e7
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-06-21 08:07:40 +0200

    CorsConfig til Kotlin

--- src/main/java/no/nav/sbl/config/CorsConfig.kt
+++ src/main/java/no/nav/sbl/config/CorsConfig.kt
@@ -1,26 +1,21 @@
-package no.nav.sbl.config;
+package no.nav.sbl.config
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
-import java.util.List;
+object CorsConfig {
+    fun allowAllCorsConfig(): CorsConfigurationSource {
+        val corsConfiguration =
+            CorsConfiguration()
+                .apply {
+                    allowCredentials = true
+                    allowedHeaders = listOf("Accept", "Accept-language", "Content-Language", "Content-Type")
+                    addAllowedOriginPattern(CorsConfiguration.ALL)
+                    maxAge = 3600L
+                    allowedMethods = listOf("GET", "HEAD", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")
+                }
 
-public class CorsConfig {
-    public static CorsConfigurationSource allowAllCorsConfig() {
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(List.of("Accept", "Accept-language", "Content-Language", "Content-Type"));
-        configuration.addAllowedOriginPattern(CorsConfiguration.ALL);
-        configuration.setMaxAge(3600L);
-        configuration.setAllowedMethods(List.of("GET", "HEAD", "PUT", "POST", "PATCH", "DELETE", "OPTIONS"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+        return UrlBasedCorsConfigurationSource().apply { registerCorsConfiguration("/**", corsConfiguration) }
     }
 }
