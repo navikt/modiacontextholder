eb8a3f7a9365aa9b63e510680ddeec33306c5570
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-06-21 08:48:20 +0200

    RSEvents til Kotlin

--- src/main/java/no/nav/sbl/rest/domain/RSEvents.kt
+++ src/main/java/no/nav/sbl/rest/domain/RSEvents.kt
@@ -1,15 +1,5 @@
-package no.nav.sbl.rest.domain;
+package no.nav.sbl.rest.domain
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.experimental.Accessors;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Data
-@Accessors(fluent = true)
-@EqualsAndHashCode
-public class RSEvents {
-    public List<RSEvent> events = new ArrayList<>();
-}
+data class RSEvents(
+    val events: List<RSEvent>,
+)
