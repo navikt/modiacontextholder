6ed380b017ebe052fab7a7baefdb30be22916488
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-06-21 08:54:21 +0200

    ApplicationConfigTest til Kotlin

--- src/test/java/no/nav/sbl/ApplicationConfigTest.kt
+++ src/test/java/no/nav/sbl/ApplicationConfigTest.kt
@@ -1,23 +1,22 @@
-package no.nav.sbl;
+package no.nav.sbl
 
-import no.nav.common.nais.NaisYamlUtils;
-import no.nav.common.utils.EnvironmentUtils;
-import org.junit.Test;
+import no.nav.common.nais.NaisYamlUtils
+import no.nav.common.utils.EnvironmentUtils
+import org.junit.Test
+import java.net.URLClassLoader
 
-import java.net.URL;
-import java.net.URLClassLoader;
-
-public class ApplicationConfigTest {
+class ApplicationConfigTest {
     @Test
-    public void appconfigKanLastesFraQ0Config() throws Exception {
-        injectAzureProperties();
-        NaisYamlUtils.loadFromYaml(".nais/nais.yaml");
-        
-        new URLClassLoader(new URL[]{}).loadClass("no.nav.sbl.config.ApplicationConfig").newInstance();
+    @Throws(Exception::class)
+    fun appconfigKanLastesFraQ0Config() {
+        injectAzureProperties()
+        NaisYamlUtils.loadFromYaml(".nais/nais.yaml")
+
+        URLClassLoader(arrayOf()).loadClass("no.nav.sbl.config.ApplicationConfig").newInstance()
     }
 
-    private void injectAzureProperties() {
-        EnvironmentUtils.setProperty("AZURE_APP_WELL_KNOWN_URL", "http://dummy.io", EnvironmentUtils.Type.PUBLIC);
-        EnvironmentUtils.setProperty("AZURE_APP_CLIENT_ID", "clientid", EnvironmentUtils.Type.PUBLIC);
+    private fun injectAzureProperties() {
+        EnvironmentUtils.setProperty("AZURE_APP_WELL_KNOWN_URL", "http://dummy.io", EnvironmentUtils.Type.PUBLIC)
+        EnvironmentUtils.setProperty("AZURE_APP_CLIENT_ID", "clientid", EnvironmentUtils.Type.PUBLIC)
     }
 }
