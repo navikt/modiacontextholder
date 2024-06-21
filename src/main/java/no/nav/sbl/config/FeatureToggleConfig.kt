dce851452cd23978d01325a49e39b27a23d5a3fc
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-06-21 08:12:57 +0200

    FeatureToggleConfig til Kotlin

--- src/main/java/no/nav/sbl/config/FeatureToggleConfig.kt
+++ src/main/java/no/nav/sbl/config/FeatureToggleConfig.kt
@@ -1,44 +1,45 @@
-package no.nav.sbl.config;
+package no.nav.sbl.config
 
-import io.getunleash.DefaultUnleash;
-import io.getunleash.Unleash;
-import io.getunleash.UnleashContextProvider;
-import io.getunleash.util.UnleashConfig;
-import no.nav.common.utils.EnvironmentUtils;
-import no.nav.sbl.service.AuthContextService;
-import no.nav.sbl.service.unleash.UnleashContextProviderImpl;
-import no.nav.sbl.service.unleash.ToggleableFeatureService;
-import no.nav.sbl.service.unleash.UnleashService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.getunleash.DefaultUnleash
+import io.getunleash.Unleash
+import io.getunleash.UnleashContextProvider
+import io.getunleash.util.UnleashConfig
+import no.nav.common.utils.EnvironmentUtils
+import no.nav.sbl.service.AuthContextService
+import no.nav.sbl.service.unleash.ToggleableFeatureService
+import no.nav.sbl.service.unleash.UnleashContextProviderImpl
+import no.nav.sbl.service.unleash.UnleashService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @Configuration
-public class FeatureToggleConfig {
-
-    String api = EnvironmentUtils.getRequiredProperty("UNLEASH_SERVER_API_URL") + "/api";
-    String apiToken = EnvironmentUtils.getRequiredProperty("UNLEASH_SERVER_API_TOKEN");
+open class FeatureToggleConfig {
+    private val api: String = EnvironmentUtils.getRequiredProperty("UNLEASH_SERVER_API_URL") + "/api"
+    private val apiToken: String = EnvironmentUtils.getRequiredProperty("UNLEASH_SERVER_API_TOKEN")
 
     @Bean
-    public ToggleableFeatureService unleashService(UnleashContextProvider unleashContextProvider) {
-        UnleashConfig unleashConfig = UnleashConfig.builder()
-                .appName("modiacontextholder")
-                .environment(System.getProperty("UNLEASH_ENVIRONMENT"))
-                .instanceId(System.getProperty("APP_ENVIRONMENT_NAME", "local"))
-                .unleashAPI(api)
-                .apiKey(apiToken)
-                .unleashContextProvider(unleashContextProvider)
-                .synchronousFetchOnInitialisation(true)
-                .build();
+    open fun unleashService(unleashContextProvider: UnleashContextProvider): ToggleableFeatureService {
+        val unleashConfig =
+            UnleashConfig
+                .builder()
+                .apply {
+                    appName("modiacontextholder")
+                    environment(System.getProperty("UNLEASH_ENVIRONMENT"))
+                    instanceId(System.getProperty("APP_ENVIRONMENT_NAME", "local"))
+                    unleashAPI(api)
+                    apiKey(apiToken)
+                    unleashContextProvider(unleashContextProvider)
+                    synchronousFetchOnInitialisation(true)
+                }.build()
 
-        Unleash defaultUnleash = new DefaultUnleash(unleashConfig);
+        val defaultUnleash: Unleash = DefaultUnleash(unleashConfig)
 
-        return new UnleashService(defaultUnleash);
+        return UnleashService(defaultUnleash)
     }
 
     @Bean
     @Autowired
-    public UnleashContextProvider unleashContextProvider(AuthContextService authContextService) {
-        return new UnleashContextProviderImpl(authContextService);
-    }
+    open fun unleashContextProvider(authContextService: AuthContextService): UnleashContextProvider =
+        UnleashContextProviderImpl(authContextService)
 }
