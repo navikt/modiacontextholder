90b806330cca7941836f0dc0cafcc88f8b970275
Ludvig Hozman <ludvig123@gmail.com>
2024-06-28 16:24:04 +0200

    Fjerne contextRelative og legg til flere tester

--- src/test/java/no/nav/sbl/rest/ContextPathRedirectTest.kt
+++ src/test/java/no/nav/sbl/rest/ContextPathRedirectTest.kt
@@ -1,20 +1,45 @@
 package no.nav.sbl.rest
 
+import org.junit.Before
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.mock.web.MockServletContext
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
-@WebMvcTest(ContextPathRedirect::class)
-class ContextPathRedirectTest  {
+@WebMvcTest(
+    ContextPathRedirect::class,
+)
+@TestPropertySource(locations = ["classpath:application.properties"])
+class ContextPathRedirectTest {
     @Autowired
     lateinit var mockMvc: MockMvc
+
+    @Before
+    fun setUp() {
+        (mockMvc.dispatcherServlet.servletContext as MockServletContext).contextPath = "/modiacontextholder"
+    }
 
     @Test
     fun `Redirects requests still using the context path correctly`() {
         mockMvc.get("/modiacontextholder/test/path").andExpect {
             redirectedUrl("/test/path")
+        }
+    }
+
+    @Test
+    fun `Works with context path`() {
+        mockMvc.get("/modiacontextholder/modiacontextholder/test/path") { contextPath = "/modiacontextholder" }.andExpect {
+            redirectedUrl("/modiacontextholder/test/path")
+        }
+    }
+
+    @Test
+    fun `Does not redirect requests on the context path`() {
+        mockMvc.get("/modiacontextholder/test/path") { contextPath = "/modiacontextholder" }.andExpect {
+            status { isNotFound() }
         }
     }
 }
