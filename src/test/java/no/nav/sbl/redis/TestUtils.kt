dd67033524f96d718c86cdb8baa2c06e6ca8a09b
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-08-07 14:41:30 +0200

    Reformat

--- src/test/java/no/nav/sbl/redis/TestUtils.kt
+++ src/test/java/no/nav/sbl/redis/TestUtils.kt
@@ -15,7 +15,6 @@ import java.time.Duration
 import java.util.*
 
 object TestUtils {
-
     class RedisContainer : GenericContainer<RedisContainer>("redis:6-alpine") {
         init {
             withCommand("redis-server --requirepass $PASSWORD")
@@ -23,11 +22,17 @@ object TestUtils {
         }
     }
 
-    class RedisSubscriber(private val subscribeCallback: () -> Unit = {}) : JedisPubSub() {
+    class RedisSubscriber(
+        private val subscribeCallback: () -> Unit = {},
+    ) : JedisPubSub() {
         val messages: MutableList<Pair<String, String>> = mutableListOf()
         private val lock = WaitLock(false)
 
-        override fun onPMessage(pattern: String, channel: String, message: String) {
+        override fun onPMessage(
+            pattern: String,
+            channel: String,
+            message: String,
+        ) {
             val messagePair = Pair(channel, message)
             messages.add(messagePair)
             if (messagePair == lock.keyOwner) {
@@ -35,11 +40,17 @@ object TestUtils {
             }
         }
 
-        override fun onPSubscribe(pattern: String?, subscribedChannels: Int) {
+        override fun onPSubscribe(
+            pattern: String?,
+            subscribedChannels: Int,
+        ) {
             subscribeCallback()
         }
 
-        suspend fun assertReceivedMessage(channel: String, message: String) {
+        suspend fun assertReceivedMessage(
+            channel: String,
+            message: String,
+        ) {
             val messagePair = Pair(channel, message)
             if (!messages.contains(messagePair)) {
                 lock.lock(messagePair)
@@ -49,7 +60,6 @@ object TestUtils {
     }
 
     open class WithRedis {
-
         companion object {
             private var job: Job? = null
             private var subscriber: RedisSubscriber? = null
@@ -71,17 +81,19 @@ object TestUtils {
         }
 
         @Before
-        fun setupSubscriber() = runBlocking {
-            val lock = WaitLock(true)
-            subscriber = RedisSubscriber { lock.unlock() }
-            val hostAndPort = redisHostAndPort()
-            val jedis = Jedis(hostAndPort)
-            jedis.auth(PASSWORD)
-            job = GlobalScope.launch {
-                jedis.psubscribe(subscriber, "*")
+        fun setupSubscriber() =
+            runBlocking {
+                val lock = WaitLock(true)
+                subscriber = RedisSubscriber { lock.unlock() }
+                val hostAndPort = redisHostAndPort()
+                val jedis = Jedis(hostAndPort)
+                jedis.auth(PASSWORD)
+                job =
+                    GlobalScope.launch {
+                        jedis.psubscribe(subscriber, "*")
+                    }
+                lock.waitForUnlock()
             }
-            lock.waitForUnlock()
-        }
 
         @After
         fun closeSubscriber() {
@@ -95,14 +107,20 @@ object TestUtils {
 
         fun getMessages() = subscriber?.messages ?: emptyList<Pair<String, String>>()
 
-        suspend fun assertReceivedMessage(channel: String, message: String, timeout: Duration = Duration.ofSeconds(1)) {
+        suspend fun assertReceivedMessage(
+            channel: String,
+            message: String,
+            timeout: Duration = Duration.ofSeconds(1),
+        ) {
             withTimeout(timeout.toMillis()) {
                 subscriber?.assertReceivedMessage(channel, message)
             }
         }
     }
 
-    class WaitLock(initiallyLocked: Boolean = false) {
+    class WaitLock(
+        initiallyLocked: Boolean = false,
+    ) {
         private val mutex = Mutex(initiallyLocked)
         var keyOwner: Any? = null
 
