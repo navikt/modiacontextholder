46c5a74e3fd39c28e8cbc940ea2913a8393f9e0e
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-08-07 11:06:29 +0200

    Legg til integrasjonstest

--- src/main/java/no/nav/sbl/config/WebSocketConfiguration.kt
+++ src/main/java/no/nav/sbl/config/WebSocketConfiguration.kt
@@ -1,23 +1,17 @@
 package no.nav.sbl.config
 
 import no.nav.sbl.websocket.ContextWebSocketHandler
+import no.nav.sbl.websocket.WebSocketRegistry
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.socket.WebSocketHandler
 import org.springframework.web.socket.config.annotation.EnableWebSocket
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
 @EnableWebSocket
-open class WebSocketConfiguration(
-    private val webSocketHandler: WebSocketHandler,
-) : WebSocketConfigurer {
-    override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
-        registry
-            .addHandler(webSocketHandler, "/ws/{ident}")
-            .setAllowedOrigins("*")
-    }
+open class WebSocketConfiguration {
+    @Bean
+    open fun webSocketRegistry(contextWebSocketHandler: ContextWebSocketHandler): WebSocketRegistry =
+        WebSocketRegistry(contextWebSocketHandler)
 
     @Bean
     open fun webSocketHandler(): ContextWebSocketHandler = ContextWebSocketHandler()
