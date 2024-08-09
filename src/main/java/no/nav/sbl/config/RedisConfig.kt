a1bd3c4bfab6450aa07a60f4548c8fd263371757
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-08-09 13:27:25 +0200

    Legg på debuglogging

--- src/main/java/no/nav/sbl/config/RedisConfig.kt
+++ src/main/java/no/nav/sbl/config/RedisConfig.kt
@@ -61,7 +61,11 @@ open class RedisConfig {
             runCatching {
                 objectMapper.readValue(message, RSEvent::class.java)
             }.fold(
-                onSuccess = { contextEventPublisher.publishMessage(it.veilederIdent, it.eventType) },
+                onSuccess = {
+                    // Debug
+                    log.info("Redis-melding mottatt på kanal $channel med eventType ${it.eventType}")
+                    contextEventPublisher.publishMessage(it.veilederIdent, it.eventType)
+                },
                 onFailure = { log.error("Feil ved deserialisering av Redis-melding på kanal $channel", it) },
             )
         }
