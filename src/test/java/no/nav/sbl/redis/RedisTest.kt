c1a51cc07b2912da546402c19cd74bad410c26a0
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-08-09 14:10:02 +0200

    Test RedisSubscriber

--- src/test/java/no/nav/sbl/redis/RedisTest.kt
+++ src/test/java/no/nav/sbl/redis/RedisTest.kt
@@ -1,23 +1,26 @@
 package no.nav.sbl.redis
 
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.assertTrue
 
 class RedisTest : TestUtils.WithRedis() {
+    private val hostAndPort = redisHostAndPort()
+    private val authJedisPool =
+        AuthJedisPool(
+            uriWithAuth =
+                RedisUriWithAuth(
+                    uri = "redis://${hostAndPort.host}:${hostAndPort.port}",
+                    user = "default",
+                    password = PASSWORD,
+                ),
+        )
+
     @Test
     fun `sender redis-meldinger`() =
         runBlocking {
-            val hostAndPort = redisHostAndPort()
-            val authJedisPool =
-                AuthJedisPool(
-                    uriWithAuth =
-                        RedisUriWithAuth(
-                            uri = "redis://${hostAndPort.host}:${hostAndPort.port}",
-                            user = "default",
-                            password = PASSWORD,
-                        ),
-                )
             val redisPublisher = RedisPublisher(authJedisPool, "TestChannel")
             redisPublisher.publishMessage("TestMessage1")
             redisPublisher.publishMessage("TestMessage2")
@@ -26,5 +29,21 @@ class RedisTest : TestUtils.WithRedis() {
             assertReceivedMessage("TestChannel", "TestMessage2")
             assertReceivedMessage("TestChannel", "TestMessage1")
             assertTrue(getMessages().size == 3)
+        }
+
+    @Test
+    fun `redis subscriber mottar meldinger`() =
+        runBlocking {
+            val defferedMessage = CompletableDeferred<String>()
+            val redisSubscription =
+                RedisSubscription("TestChannel") { _, message ->
+                    defferedMessage.complete(message)
+                }
+            val redisSubscriber = RedisSubscriber(authJedisPool, listOf(redisSubscription))
+            redisSubscriber.start()
+            val redisPublisher = RedisPublisher(authJedisPool, "TestChannel")
+            redisPublisher.publishMessage("TestMessage")
+            assertThat(defferedMessage.await()).isEqualTo("TestMessage")
+            redisSubscriber.stop()
         }
 }
