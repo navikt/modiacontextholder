7619e5bf4c2a68c9384158755250adb7aef5863a
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-08-09 11:40:54 +0200

    Legg til Redisabonnement

--- src/main/java/no/nav/sbl/config/RedisConfig.kt
+++ src/main/java/no/nav/sbl/config/RedisConfig.kt
@@ -1,9 +1,20 @@
-package no.nav.sbl.redis
+package no.nav.sbl.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.common.utils.EnvironmentUtils
+import no.nav.sbl.redis.AuthJedisPool
+import no.nav.sbl.redis.RedisPersistence
+import no.nav.sbl.redis.RedisPublisher
+import no.nav.sbl.redis.RedisSubscriber
+import no.nav.sbl.redis.RedisSubscription
+import no.nav.sbl.redis.RedisUriWithAuth
+import no.nav.sbl.redis.RedisVeilederContextDatabase
+import no.nav.sbl.redis.VeilederContextDatabase
+import no.nav.sbl.rest.model.RSEvent
+import no.nav.sbl.websocket.WebSocketContextEventPublisher
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import kotlin.jvm.optionals.getOrDefault
@@ -20,6 +31,12 @@ open class RedisConfig {
     open fun redisPublisher(authJedisPool: AuthJedisPool): RedisPublisher = RedisPublisher(authJedisPool, channel)
 
     @Bean
+    open fun redisSubscriber(
+        authJedisPool: AuthJedisPool,
+        redisSubscriptions: List<RedisSubscription>,
+    ): RedisSubscriber = RedisSubscriber(authJedisPool, redisSubscriptions)
+
+    @Bean
     open fun authJedisPool(): AuthJedisPool = AuthJedisPool(RedisUriWithAuth(uri = redisUri, user = redisUser, password = redisPassword))
 
     @Bean
@@ -30,4 +47,23 @@ open class RedisConfig {
         authJedisPool: AuthJedisPool,
         objectMapper: ObjectMapper = jacksonObjectMapper().registerModule(JavaTimeModule()),
     ): VeilederContextDatabase = RedisVeilederContextDatabase(authJedisPool, objectMapper)
+
+    @Bean
+    open fun contextEventRedisSubscription(
+        contextEventPublisher: WebSocketContextEventPublisher,
+        objectMapper: ObjectMapper = jacksonObjectMapper().registerModule(JavaTimeModule()),
+    ): RedisSubscription {
+        val log = LoggerFactory.getLogger(RedisSubscription::class.java)
+
+        return RedisSubscription(
+            channel,
+        ) { channel, message ->
+            runCatching {
+                objectMapper.readValue(message, RSEvent::class.java)
+            }.fold(
+                onSuccess = { contextEventPublisher.publishMessage(it.veilederIdent, it.eventType) },
+                onFailure = { log.error("Feil ved deserialisering av Redis-melding på kanal $channel", it) },
+            )
+        }
+    }
 }
--- src/main/java/no/nav/sbl/websocket/WebSocketContextEventPublisher.kt
+++ src/main/java/no/nav/sbl/websocket/WebSocketContextEventPublisher.kt
@@ -1,13 +1,12 @@
 package no.nav.sbl.websocket
 
-import no.nav.sbl.service.ContextEventPublisher
 import org.springframework.stereotype.Component
 
 @Component
 class WebSocketContextEventPublisher(
     private val contextWebSocketHandler: ContextWebSocketHandler,
-) : ContextEventPublisher {
-    override fun publishMessage(
+) {
+    fun publishMessage(
         ident: String,
         eventType: String,
     ) {
