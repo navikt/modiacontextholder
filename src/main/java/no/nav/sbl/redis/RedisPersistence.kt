5fedd51ecd293ae4aa302696f7f91cb9d2f618b8
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-08-12 15:54:09 +0200

    Revert "Legg til Redisabonnement (#359)"
    This reverts commit 19e75157122b9fb3220ae29ab3fb3112a74ba29e.
    
    Revert "Legg på debuglogging"
    
    This reverts commit a1bd3c4bfab6450aa07a60f4548c8fd263371757.
    
    Revert "Test RedisSubscriber"
    
    This reverts commit c1a51cc07b2912da546402c19cd74bad410c26a0.
    
    Revert "Bruk SmartLifecycle"
    
    This reverts commit 2335f6c08a0df39d58ed169658d418420b39e38b.
    
    Revert "Fjern debuglog"
    
    This reverts commit 05f5540079fb94f337ee56cdc0c45787645e6e76.
    
    Revert "Bruk JedisPooled"
    
    This reverts commit 936ceacd88f9b0f711c20bada5a7ab08b52b6e9f.
    
    Revert "Legg til HealthCheck"
    
    This reverts commit 54fba61d3d7fd7618d9a38a358ecca39aee2df6b.
    
    Revert "Fjern prefix fra host"
    
    This reverts commit 0aa5cae575d07edcb57c29968441b29fbd86a9c3.
    
    Revert "Bytt tilbake til JedisPool"
    
    This reverts commit fa5b6dd6ca2b317f65895f6ffc9620bab77cf9e8.
    
    Revert "Fiks RedisSubscriber stop"
    
    This reverts commit ebeb855b051754a630b78bfbcc34f6e6d6d7b901.
    
    Revert "Log"
    
    This reverts commit aeb2348bb94ee79a0fece24d6366b5caff2a44b2.
    
    Revert "Catch exception"
    
    This reverts commit e2c433eb4ac120c5c8373cb81d2d0c35e9c25994.
    
    Revert "Flytt health check til rest"
    
    This reverts commit faa57cc4cfc8928a6286f8e21ad641ff69c198a1.
    
    Revert "Sett uendelig timeout på pool"
    
    This reverts commit 87a2be38e913cb637ce0b5f2e2cc101e8b299c36.

--- src/main/java/no/nav/sbl/redis/RedisPersistence.kt
+++ src/main/java/no/nav/sbl/redis/RedisPersistence.kt
@@ -1,52 +1,52 @@
 package no.nav.sbl.redis
 
+import kotlinx.coroutines.*
 import no.nav.common.health.HealthCheck
 import no.nav.common.health.HealthCheckResult
 import no.nav.common.health.selftest.SelfTestCheck
 import no.nav.sbl.config.Pingable
-import redis.clients.jedis.JedisPool
-import java.util.*
+import java.util.UUID
+import kotlin.concurrent.fixedRateTimer
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
 
 class RedisPersistence(
-    private val jedisPool: JedisPool,
+    private val redisPool: AuthJedisPool,
     private val codeGenerator: CodeGenerator = UUIDGenerator(),
     private val expiration: Duration = 10.minutes,
     private val scope: String = "fnr-code",
 ) : HealthCheck,
     Pingable {
-    fun getFnr(code: String): Result<String?> =
-        runCatching {
-            val key = getKey(code)
-            jedisPool.resource.use { jedis -> jedis.get(key) }
+    init {
+        fixedRateTimer("Redis check", daemon = true, initialDelay = 0, period = 10.seconds.inWholeMilliseconds) {
+            runBlocking(Dispatchers.IO) { ping() }
         }
+    }
 
-    fun generateAndStoreTempCodeForFnr(fnr: String): TempCodeResult {
+    suspend fun getFnr(code: String): Result<String?> {
+        val key = getKey(code)
+        return redisPool.useResource {
+            it.get(key)
+        }
+    }
+
+    suspend fun generateAndStoreTempCodeForFnr(fnr: String): TempCodeResult {
         val code = codeGenerator.generateCode(fnr)
         val key = getKey(code)
         val result =
-            runCatching {
-                jedisPool.resource.use { jedis -> jedis.setex(key, expiration.inWholeSeconds, fnr) }
+            redisPool.useResource {
+                it.setex(key, expiration.inWholeSeconds, fnr)
             }
         return TempCodeResult(result, code)
     }
 
     private fun getKey(code: String): String = "$scope-$code"
 
-    private fun pingRedis(): Result<String?> =
-        runCatching {
-            jedisPool.resource.use { jedis ->
-                val pong = jedis.ping()
-                if (pong != "PONG") {
-                    throw IllegalStateException("Redis ping feilet")
-                }
-                pong
-            }
-        }
+    private suspend fun pingRedis(): Result<String?> = redisPool.useResource { it.ping() }
 
     override fun checkHealth(): HealthCheckResult {
-        val result = pingRedis()
+        val result = runBlocking { pingRedis() }
         return if (result.isSuccess) {
             HealthCheckResult.healthy()
         } else {
