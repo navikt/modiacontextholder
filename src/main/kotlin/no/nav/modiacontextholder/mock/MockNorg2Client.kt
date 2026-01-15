4966645053b92b426a2268c2f77d5a1f910a310e
martinsomm-nav <martin.dagfinn.boler.sommerseth@nav.no>
2026-01-15 14:23:20 +0100

    Oppdaterer documentation.yaml og litt tester for å få med enhetstype-attributtet

--- src/main/kotlin/no/nav/modiacontextholder/mock/MockNorg2Client.kt
+++ src/main/kotlin/no/nav/modiacontextholder/mock/MockNorg2Client.kt
@@ -6,8 +6,8 @@ import no.nav.modiacontextholder.consumers.norg2.domain.Enhet
 class MockNorg2Client : Norg2Client {
     override fun hentAlleEnheter(): List<Enhet> =
         listOf(
-            Enhet("0001", "Test enhet", "test", "0"),
-            Enhet("0002", "Test annen enhet", "test", "1"),
-            Enhet("9999", "Test enhet", "test", "2"),
+            Enhet("0001", "Test enhet", "test", "LOKAL", "0"),
+            Enhet("0002", "Test annen enhet", "test", null, "1"),
+            Enhet("9999", "Test enhet", "test", "KO", "2"),
         )
 }
